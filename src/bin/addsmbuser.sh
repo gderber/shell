7c4134ce97178ccda31861708d580fdf354e6305
G S Derber <gd.smlinux@gmail.com>
2018-07-08 20:13:22 +0000

    Update addsmbuser

--- src/bin/addsmbuser.sh
+++ src/bin/addsmbuser.sh
@@ -1,39 +1,106 @@
 #!/bin/bash
+# ======================================================================
+#
+# Add Smb User
+#
+# Geoff S Derber
+#
+#
+# ======================================================================
 
-GROUPLIST="hoid_g bdarklighter gdarklighter jkholin mcauthun morpheus_g palpatine thrawn trinity ealvere ssanche lmandragoran etrekand tmerrilyn pababra "
-USER_LIST="hoid biggs gavin jasnah matrim morpheus palpatine thrawn trinity egwane suian lan elaine thom perrin"
-IDNUM_LIST="10024 10025 10026 10027 10028 10029 10030 10031 10032 10033"
-SNLIST="hoid darklighter darklighter kholin cauthun morpheus palpatine thrawn trinity alvere sanche mandragoran trekand merrilyn ababra"
-INLIST="hh bd gd jk mc mm tt tt ea ss lm et tm pa"
+# ======================================================================
+#
+# function getmaxuid
+function getmaxuid () {
+    #IDNUM=$(ldapsearch -H ldaps://your-ldap-domain -D "cn=Manager,dc=domain,dc=com" -W | awk '/uidNumber: / {print $2}' | sort | tail -n 1)
+    IDNUM=10026
+    echo -ne ${IDNUM}
+}
 
-GPS=($GROUPLIST)
-USERS=($USER_LIST)
-IDNUMS=($IDNUM_LIST)
-INS=($INLIST)
-SNS=($SNLIST)
+function getusername () {
+    GN=${1}
+    SN=${2}
+    if [[ -z ${SN} ]]; then
+	USERNAME=${GN}
+    else
+	FI=$(echo ${GN}|cut -c1|tr [A-Z] [a-z]}
+	USERNAME=${FI}${SN}
+    fi
+    echo -ne ${USERNAME}
+}
 
-echo ${GPS}
-NUM=${#GPS[*]}
-X=0
+function getdomain () {
+    DOMAIN=$(grep workgroup /etc/samba/smb.conf|cut -d= -f2|cut -d\  -f2) #sed to strip spaces would be better
+    echo -ne ${DOMAIN}
+}
 
-echo "Num = ${NUM}"
-
-while [[ ${X} -lt ${NUM} ]]  
-do
-    GP=${GPS[${X}]}
-    USR=${USERS[${X}]}
-    IDNUM=${IDNUMS[${X}]}
-    IN=${INS[${X}]}
-    SN=${SNS[${X}]}
-    echo "GROUP = ${GP}"
-    echo "USER  = ${USR}"
-    echo "IDNUM = ${IDNUM}"
-    echo "Surname = ${SN}"
-    echo "Initials = ${IN}"
-
-    sudo samba-tool group add ${GP} --gid-number ${IDNUM} --nis-domain=Olympus -U Administrator
-    sudo samba-tool user create ${USR} --nis-domain=olympus --surname=${SN} --given-name=${USR} --initials=${IN} --unix-home=/home/samba/users/${GP} --uid-number=${IDNUM} --login-shell=/bin/bash --gid-number=${IDNUM} -U Administrator
-
-    X=($X+1)
-done
+function addusers () {
+    IDNUM=$(getmaxuid)
+    DOMAIN=${DOMAIN}
+    INPUTFILE=userlist
     
+    while IFS='' read -r LINE || [[ -n "$LINE" ]]
+    do
+	GN=$(echo ${LINE}|cut -d, -f1)
+	SN=$(echo ${LINE}|cut -d, -f2)
+       	IN=$(echo ${LINE}|cut -d, -f3)
+	NICK=$(echo ${LINE}|cut -d, -f4)
+
+	if [[ -n ${NICK} ]]; then
+	    USERNAME=${NICK}
+	else
+	    USERNAME=$(getusername)
+	fi
+       	GROUPNAME=${USERNAME}_g
+    
+	PPATH=$(echo ${LINE}|cut -d, -f1)
+	SPATH=$(echo ${LINE}|cut -d, -f1)
+	DLTR=$(echo ${LINE}|cut -d, -f1)
+
+	[[ -n ${SN} ]] && SURNAME="--surname=${SN}"
+	[[ -n ${IN} ]] && INITIALS="--initials=${IN}"
+	[[ -n ${PPATH} ]] && PROFILEPATH="--profile-path=${PPATH}"
+	[[ -n ${DRIVELTR} ]] && DLTR="--home-drive=${DRIVELTR}"
+	[[ -n ${HPATH} ]] && HDIR="--home-directory=${HPATH}"
+	[[ -n ${SPATH} ]] && SPATH="--script-path=${SPATH}"
+	[[ -n ${TITLE} ]] && TITLE="--job-title=${TITLE}"
+	[[ -n ${DEPT} ]] && DEPT="--department=${DEPT}"
+	[[ -n ${COMPANY} ]] && COMPANY="--company=${COMPANY}"
+	[[ -n ${DESC} ]] && DESC="--description=${DESCRIPTION}"
+	[[ -n ${EMAIL} ]] && EMAIL="--mail-address=${EMAIL}"
+	[[ -n ${WEBSITE} ]] && WEBSITE="--internet-address=${WEBSITE}"
+	[[ -n ${PHONE} ]] && PHONE="--telephone-number=${PHONE}"
+	[[ -n ${OFFICE} ]] && OFFICE="--physical-delivery-office=${OFFICE}"
+	[[ -n ${GECOS} ]] && GECOS="--gecos=${GECOS}"
+
+	OPT="${SURNAME} ${INITIALS} ${PROFILEPATH} ${DLTR} ${HDIR} ${SPATH} ${TITLE} ${DEPT} ${COMPANY} ${DESC} ${EMAIL} ${WEBSITE} ${PHONE} ${OFFICE} ${GECOS}"
+
+	echo "GROUP = ${GROUPNAME}"
+	echo "USER  = ${USERNAME}"
+	echo "IDNUM = ${IDNUM}"
+
+	sudo samba-tool group \
+	     add ${GROUPNAME} \
+	     --gid-number ${IDNUM} \
+	     --nis-domain=${DOMAIN} \
+	     -U Administrator
+	
+	sudo samba-tool user \
+	     create ${USERNAME} \
+	     --given-name=${GN} \
+	     --nis-domain=${DOMAIN} \
+	     --unix-home=/home/${USERNAME} \
+	     --uid-number=${IDNUM} \
+	     --gid-number=${IDNUM} \
+	     --login-shell=/bin/bash \
+	     ${OPTS} -U Administrator
+
+	IDNUM=($IDNUM+1)
+    done < ${INPUTFILE}
+}
+    
+function main () {
+    addusers
+}
+
+main $@
